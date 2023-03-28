198058fd441df8274203785f00b7e975b30f59e2
Jms147 <steinhauserjames3@gmail.com>
2023-03-27 20:28:02 -0700

    added screen shot

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,135 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+
+
+var saveBtn = $('#saveBtn')
+var description = [
+    {
+      id: "0",
+      hour: "09",
+      time: "09",
+      meridiem: "am",
+      reminder: ""
+  },
+  {
+      id: "1",
+      hour: "10",
+      time: "10",
+      meridiem: "am",
+      reminder: ""
+  },
+  {
+      id: "2",
+      hour: "11",
+      time: "11",
+      meridiem: "am",
+      reminder: ""
+  },
+  {
+      id: "3",
+      hour: "12",
+      time: "12",
+      meridiem: "pm",
+      reminder: ""
+  },
+  {
+      id: "4",
+      hour: "01",
+      time: "13",
+      meridiem: "pm",
+      reminder: ""
+  },
+  {
+      id: "5",
+      hour: "02",
+      time: "14",
+      meridiem: "pm",
+      reminder: ""
+  },
+  {
+      id: "6",
+      hour: "03",
+      time: "15",
+      meridiem: "pm",
+      reminder: ""
+  },
+  {
+      id: "7",
+      hour: "04",
+      time: "16",
+      meridiem: "pm",
+      reminder: ""
+  },
+  {
+      id: "8",
+      hour: "05",
+      time: "17",
+      meridiem: "pm",
+      reminder: ""
+  },
+  
+]
+  
+$(function() {
+saveBtn.on('click', function() {
+  localStorage.setItem("description", JSON.stringify(description))
+})  
+  });
+function renderReminders(){
+var reminders = JSON.parse(localStorage.getItem(reminders));
+$('#hour-8 .description').val(localStorage.getItem('0'));
+$('#hour-9 .description').val(localStorage.getItem('1'));
+$('#hour-10 .description').val(localStorage.getItem('2'));
+$('#hour-11 .description').val(localStorage.getItem('3'));
+$('#hour-12 .description').val(localStorage.getItem('4'));
+$('#hour-1 .description').val(localStorage.getItem('5'));
+$('#hour-2 .description').val(localStorage.getItem('6'));
+$('#hour-3 .description').val(localStorage.getItem('7'));
+$('#hour-4 .description').val(localStorage.getItem('8'));
+$('#hour-5 .description').val(localStorage.getItem('9'));
+}
+function init() {
+  var storedDescription = JSON.parse(localStorage.getItem(description));
+  renderReminders();
+}
+
+
+  var currentHour = $(this).data('hour')
+  $(description).each(function () {
+    var timeDiv = $(this).attr('id')
+    if (currentHour == timeDiv) {
+      $(this).removeClass("future")
+      $(this).removeClass("past")
+      $(this).addClass("present")
+
+    } else if (currentHour < timeDiv) {
+      $(this).removeClass("present")
+      $(this).removeClass("past")
+      $(this).addClass("future")
+
+    } else if (currentHour > timeDiv) {
+      $(this).removeClass("present")
+      $(this).removeClass("future")
+      $(this).addClass("past")
+    }
+  })
+  $(".time-block").each(function () {
+    let timeDiv = $(this).data('hour')
+    if (currentHour == timeDiv) {
+      $(this).removeClass("future")
+      $(this).removeClass("past")
+      $(this).addClass("present")
+
+    } else if (currentHour < timeDiv) {
+      $(this).removeClass("present")
+      $(this).removeClass("past")
+      $(this).addClass("future")
+
+    } else if (currentHour > timeDiv) {
+      $(this).removeClass("present")
+      $(this).removeClass("future")
+      $(this).addClass("past")
+    }
+  })
+
+  var todayDate = dayjs().format("DD/MM/YYYY hh:mm:ss");
+  $("#currentDay").html(todayDate);
+
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # organizer
+
+##DESCRIPTION
+It is a day planner that is supposed to change color as time passes for past present and future. I could not figure out what I missed and it only shows present color. It is also supposed to show saved description in the local storage but I couldn't figure out how to get it to stay.
+##INSTALLATION
+jsteinhauser-24.github.io/organizer
+##CREDITS
+I used this 2 webpages to try to figure out what I was missing
+https://github.com/lbmoody/simple-day-planner/blob/master/assets/script.js
+https://stackoverflow.com/questions/72512032/how-can-i-compare-the-current-time-with-my-divs-id-900-in-javascript
+##LICENSE
+I used the MIT license
+
+##SCREENSHOT
